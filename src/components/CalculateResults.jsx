0eb9c161c63916f44b83f12b97b07c7cd643c4ce
Anjanique Mackey <anjaniquemackey@gmail.com>
2022-12-17 18:05:20 -0800

    changes calculate results of say loading

--- src/components/CalculateResults.jsx
+++ src/components/CalculateResults.jsx
@@ -2,6 +2,8 @@
 export default function CalculateResults(props) {
 
     return (
-        <h2 className="mx-5 mb-2 underline text-white text-xl relative bottom-[25px]">{props.numResults} results</h2>
+        <h2 className="mx-5 mb-2 underline text-white text-xl relative bottom-[25px]">
+            {props.numResults > 0 ? props.numResults : "loading"} results
+        </h2>
     );
 }
